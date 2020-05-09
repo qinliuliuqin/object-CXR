574c511024f0e26294d5fae0545ccc5c0ed7e0c2
Qin Liu <sjtuliuqin@163.com>
2020-05-09 14:19:24 -0400

    Minor change

--- det_train.py
+++ det_train.py
@@ -109,11 +109,8 @@ model_ft = _get_detection_model(num_classes)
 model_ft.to(device)
 
 params = [p for p in model_ft.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005,
-                            momentum=0.9, weight_decay=0.0005)
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                               step_size=5,
-                                               gamma=0.1)
+optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
 for epoch in range(num_epochs):
 
