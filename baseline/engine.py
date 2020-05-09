0bde6cdd43ad65cdde7aca60a2324946a326ed23
Qin Liu <sjtuliuqin@163.com>
2020-05-09 12:39:37 -0400

    Add debug code

--- baseline/engine.py
+++ baseline/engine.py
@@ -25,6 +25,9 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
+        # debug only
+        print(targets)
+
         loss_dict = model(images, targets)
 
         losses = sum(loss for loss in loss_dict.values())
